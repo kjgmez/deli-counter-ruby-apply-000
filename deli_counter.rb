07ee8a1dddf99d8e612cd928c9439a9f24649584
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-12 19:04:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,8 +26,12 @@ end
 def now_serving (katz_deli)
   if katz_deli.count > 0
     tbs = katz_deli[0]
+<<<<<<< HEAD
     puts "Currently serving #{tbs}."
     katz_deli.shift
+=======
+    puts "#{tbs}"
+>>>>>>> 13f0b8f197aa3450ac79dac1f175594b5515b6fd
   else
     puts "There is nobody waiting to be served!"
   end
