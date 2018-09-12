362c5584134f29660a8fe3f5b599b23a38251cda
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-12 19:24:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -35,5 +35,6 @@ end
 
 line = []
 take_a_number(line) # Welcome you are ticket number 1, and you are 1 in line.
+take_a_number(line)
 
-puts "Line is #{line}" # Line is [1]
+puts "Line is #{line}" # Line is [1, 2]
